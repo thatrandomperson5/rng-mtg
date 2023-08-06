2faa2b42324dcb376b14da829aee0a715842d3ed
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2023-08-06 16:30:32 -0500

    Make more efficient

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ async function rcard(id) {
   var cards = await fetchcards();
   let total = cards["total_cards"];
   var cardNumber = ranInt(1, total);
-  while (cardNumber > 175) {
-    let next = await fetch(cards["next_page"]);
+  if (cardNumber > 175) {
+    let page = Math.floor(cardNumber / 175)
+    let url = new URL("https://api.scryfall.com/cards/search");
+    url.searchParams.append("q", query);
+    url.searchParams.append("page", page + 1);
+    let next = await fetch(url);
     let njson = await next.json();
-    cardNumber -= 175;
+    cardNumber -= 175 * page;
     cards = njson;
   }
   let card = cards["data"][cardNumber-1];
